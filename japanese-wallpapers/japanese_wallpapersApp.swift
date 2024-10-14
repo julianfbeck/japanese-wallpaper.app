0f27a64042bcba46775bbf3dc6d0ee834b71bf8a
Julian Beck <ju-fa-beck@t-online.de>
2024-10-14 18:11:00 +0200

    Initial Commit

---
+++ japanese-wallpapers/japanese_wallpapersApp.swift
@@ -0,0 +1,17 @@
+//
+//  japanese_wallpapersApp.swift
+//  japanese-wallpapers
+//
+//  Created by Julian Beck on 14.10.24.
+//
+
+import SwiftUI
+
+@main
+struct japanese_wallpapersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
