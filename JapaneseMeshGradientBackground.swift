e242372117958fe71f29392337a7e94491838b34
Julian Beck <ju-fa-beck@t-online.de>
2024-10-22 09:19:11 +0200

    update view

--- JapaneseMeshGradientBackground.swift
+++ JapaneseMeshGradientBackground.swift
@@ -1,87 +1,77 @@
+//
+//  JapaneseMeshGradientBackground.swift
+//  japanese-wallpapers
+//
+//  Created by Julian Beck on 22.10.24.
+//
+import SwiftUI
+
 struct JapaneseMeshGradientBackground: View {
-    @Environment(\.colorScheme) private var colorScheme
-    @State private var t: Float = 0.0
-    @State private var timer: Timer?
+    @State var t: Float = 0.0
+    @State var timer: Timer?
+    @Environment(\.colorScheme) var colorScheme
     
     var body: some View {
-        Group {
-            if colorScheme == .light {
-                // Light mode: Animated white to red gradient
-                MeshGradient(width: 3, height: 3, points: [
-                    // Fixed top row
-                    .init(0, 0), .init(0.5, 0), .init(1, 0),
-                    
-                    // Animated middle and bottom rows
-                    [sinInRange(-0.2...0.2, offset: 0.439, timeScale: 0.142, t: t),
-                     sinInRange(0.3...0.7, offset: 3.42, timeScale: 0.184, t: t)],
-                    [sinInRange(0.4...0.6, offset: 0.239, timeScale: 0.084, t: t),
-                     sinInRange(0.4...0.6, offset: 5.21, timeScale: 0.142, t: t)],
-                    [sinInRange(0.8...1.2, offset: 0.939, timeScale: 0.084, t: t),
-                     sinInRange(0.3...0.7, offset: 0.25, timeScale: 0.142, t: t)],
-                    
-                    [sinInRange(-0.2...0.2, offset: 1.439, timeScale: 0.142, t: t),
-                     sinInRange(0.8...1.2, offset: 3.42, timeScale: 0.184, t: t)],
-                    [sinInRange(0.4...0.6, offset: 0.339, timeScale: 0.184, t: t),
-                     sinInRange(0.8...1.2, offset: 1.22, timeScale: 0.172, t: t)],
-                    [sinInRange(0.8...1.2, offset: 0.939, timeScale: 0.056, t: t),
-                     sinInRange(0.8...1.2, offset: 0.47, timeScale: 0.142, t: t)]
-                ], colors: [
-                    .white, .white, .white,                               // Top row: Pure white
-                    Color(red: 1.0, green: 0.9, blue: 0.9),              // Middle row
-                    Color(red: 0.98, green: 0.85, blue: 0.85),
-                    Color(red: 0.95, green: 0.8, blue: 0.8),
-                    Color(red: 0.95, green: 0.75, blue: 0.75),           // Bottom row
-                    Color(red: 0.92, green: 0.70, blue: 0.70),
-                    Color(red: 0.90, green: 0.65, blue: 0.65)
-                ])
-            } else {
-                // Dark mode: Animated black to deep red gradient
-                MeshGradient(width: 3, height: 3, points: [
-                    // Fixed top row
-                    .init(0, 0), .init(0.5, 0), .init(1, 0),
-                    
-                    // Animated middle and bottom rows
-                    [sinInRange(-0.2...0.2, offset: 0.439, timeScale: 0.142, t: t),
-                     sinInRange(0.3...0.7, offset: 3.42, timeScale: 0.184, t: t)],
-                    [sinInRange(0.4...0.6, offset: 0.239, timeScale: 0.084, t: t),
-                     sinInRange(0.4...0.6, offset: 5.21, timeScale: 0.142, t: t)],
-                    [sinInRange(0.8...1.2, offset: 0.939, timeScale: 0.084, t: t),
-                     sinInRange(0.3...0.7, offset: 0.25, timeScale: 0.142, t: t)],
-                    
-                    [sinInRange(-0.2...0.2, offset: 1.439, timeScale: 0.142, t: t),
-                     sinInRange(0.8...1.2, offset: 3.42, timeScale: 0.184, t: t)],
-                    [sinInRange(0.4...0.6, offset: 0.339, timeScale: 0.184, t: t),
-                     sinInRange(0.8...1.2, offset: 1.22, timeScale: 0.172, t: t)],
-                    [sinInRange(0.8...1.2, offset: 0.939, timeScale: 0.056, t: t),
-                     sinInRange(0.8...1.2, offset: 0.47, timeScale: 0.142, t: t)]
-                ], colors: [
-                    .black, .black, .black,                              // Top row: Pure black
-                    Color(red: 0.15, green: 0.08, blue: 0.08),          // Middle row
-                    Color(red: 0.20, green: 0.10, blue: 0.10),
-                    Color(red: 0.25, green: 0.12, blue: 0.12),
-                    Color(red: 0.25, green: 0.12, blue: 0.12),          // Bottom row
-                    Color(red: 0.30, green: 0.13, blue: 0.13),
-                    Color(red: 0.35, green: 0.15, blue: 0.15)
-                ])
-            }
-        }
-        .ignoresSafeArea()
+        MeshGradient(width: 3, height: 3, points: [
+            .init(0, 0), .init(0.5, 0), .init(1, 0),
+
+            [sinInRange(-0.8...(-0.2), offset: 0.439, timeScale: 0.342, t: t),
+             sinInRange(0.3...0.7, offset: 3.42, timeScale: 0.984, t: t)],
+            [sinInRange(0.1...0.8, offset: 0.239, timeScale: 0.084, t: t),
+             sinInRange(0.2...0.8, offset: 5.21, timeScale: 0.242, t: t)],
+            [sinInRange(1.0...1.5, offset: 0.939, timeScale: 0.084, t: t),
+             sinInRange(0.4...0.8, offset: 0.25, timeScale: 0.642, t: t)],
+            
+            [sinInRange(-0.8...0.0, offset: 1.439, timeScale: 0.442, t: t),
+             sinInRange(1.4...1.9, offset: 3.42, timeScale: 0.984, t: t)],
+            [sinInRange(0.3...0.6, offset: 0.339, timeScale: 0.784, t: t),
+             sinInRange(1.0...1.2, offset: 1.22, timeScale: 0.772, t: t)],
+            [sinInRange(1.0...1.5, offset: 0.939, timeScale: 0.056, t: t),
+             sinInRange(1.3...1.7, offset: 0.47, timeScale: 0.342, t: t)]
+        ], colors: [
+            colorScheme == .dark ? Color.black : Color.white,
+            colorScheme == .dark ? Color.black : Color.white,
+            colorScheme == .dark ? Color.black : Color.white,
+            
+            Color(red: 0.12, green: 0.06, blue: 0.12),
+            Color(red: 0.16, green: 0.08, blue: 0.16),
+            Color(red: 0.20, green: 0.10, blue: 0.20),
+            
+            Color(red: 0.24, green: 0.12, blue: 0.24),
+            Color(red: 0.28, green: 0.14, blue: 0.28),
+            Color(red: 0.32, green: 0.16, blue: 0.32)
+        ])
         .onAppear {
-            // Start the animation timer
-            timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { _ in
-                t += 0.02
-            }
+            startTimer()
         }
         .onDisappear {
-            // Clean up the timer when the view disappears
-            timer?.invalidate()
-            timer = nil
+            stopTimer()
+        }
+        .background(.black)
+        .ignoresSafeArea()
+    }
+    
+    private func startTimer() {
+        // Invalidate existing timer if any
+        stopTimer()
+        
+        // Create new timer
+        timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { _ in
+            t += 0.02
         }
     }
     
+    private func stopTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+
     func sinInRange(_ range: ClosedRange<Float>, offset: Float, timeScale: Float, t: Float) -> Float {
         let amplitude = (range.upperBound - range.lowerBound) / 2
         let midPoint = (range.upperBound + range.lowerBound) / 2
         return midPoint + amplitude * sin(timeScale * t + offset)
     }
+}
+#Preview {
+    JapaneseMeshGradientBackground()
 }
--- japanese-wallpapers/ContentView.swift
+++ japanese-wallpapers/ContentView.swift
@@ -32,26 +32,30 @@ struct ContentView: View {
     }
 }
 
+
 struct AllView: View {
     @State private var wallpaperController = WallpaperController()
 
     var body: some View {
-        NavigationStack {
-            ScrollView {
-                LazyVStack(alignment: .leading, spacing: 20) {
-                    ForEach(wallpaperController.categoriesLight) { category in
-                        CategoryWallpaperView(category: category)
+        ZStack {
+            NavigationStack {
+                ScrollView {
+                    LazyVStack(alignment: .leading, spacing: 20) {
+                        ForEach(wallpaperController.categoriesLight) { category in
+                            CategoryWallpaperView(category: category)
+                        }
                     }
+                    .padding(.vertical)
                 }
-                .padding(.vertical)
+                .navigationTitle("Wallpapers")
+                .background(JapaneseMeshGradientBackground())
             }
-            .navigationTitle("Wallpapers")
-        }
-        .refreshable {
-            await wallpaperController.fetchCategoriesLight()
-        }
-        .task {
-            await wallpaperController.fetchCategoriesLight()
+            .refreshable {
+                await wallpaperController.fetchCategoriesLight()
+            }
+            .task {
+                await wallpaperController.fetchCategoriesLight()
+            }
         }
     }
 }
